c8ec95b2186e2f0e38df4458853cb5a9765effd9
Eduarda Almeida <duda.s.almeida10@gmail.com>
2020-11-16 18:46:19 -0300

    Working

--- src/main.cpp
+++ src/main.cpp
@@ -10,27 +10,24 @@ int c=0;
 enum states{DOWN, UP, FALL, MIN, RISE, MAX};
 int state = UP;
 int on;
-float i;
+float i = 0.5;
 float T;
-
-int main() {
-    blue = 1;
-    orange = 0;
-    i = 0.5;
-    btn.rise(&rise);
-    btn.fall(&fall);
-    led = i;
-}
 
 void rise();
 void fall();
 void trn();
 void blink();
 
+int main() {
+    blue = 1;
+    orange = 0;
+    btn.rise(&rise);
+    btn.fall(&fall);
+}
+
 void rise(){
     on=1;
-    tempo();
-    b.attach(&trn, 1)
+    b.attach(&trn, 1);
 }
 
 void fall(){
@@ -51,26 +48,53 @@ void fall(){
 
 void trn(){
     if(on){
-        if(state == RISE && i == 1.0){
+        if(state == RISE && i == 1.0){  // Alcançou o MAX
             state = MAX;
             printf("STATE: MAX\n");
-        }else if(state == FALL && i = 0.0){
+        }else if(state == FALL && i == 0.0){  // Alcançou o MIN
             state = MIN;
             printf("STATE: MIN\n");
         }else{
             
-            if(state == RISE || state == UP){
+            if(state == RISE || state == UP){  // Ainda não alcançou i==0
                 state = RISE;
-                
+                printf("STATE: RISE\n");
+                i += 0.05;
             }
             
-            if(state == FALL || state == DOWN){
+            if(state == FALL || state == DOWN){  //Ainda não alcançou i==1
                 state = FALL;
-                
+                printf("STATE: FALL\n");
+                i -= 0.05;
             }
-            
-            
-            
         }
+        
+        if(i >= 1){                          // Já alcançou/passou o MAX
+            state = MAX;
+            printf("STATE: MAX\n");
+            i = 1;
+        }
+        
+        if (i <= 0){                         // á alcançou/passou o MIN
+            state = MIN;
+            printf("STATE: MIN\n");
+            i = 0;
+        }
+        led = i;
+        printf("Intensidade: %f\n", i);
+        printf("--------------------------------------------------\n");
+        t.attach(&trn, 1);
+        b.attach(&blink, 0.2);
     }
+}
+
+void blink(){
+    
+    T = 0.2;
+    
+    if(state == RISE) blue = !blue;
+    if(state == FALL) orange = !orange;
+    
+    t.attach(&blink,T);
+    
 }
